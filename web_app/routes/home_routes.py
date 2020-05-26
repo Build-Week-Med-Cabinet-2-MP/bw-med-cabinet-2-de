35c480bd80f1327be0f76fd2fd8fca87e1799b1c
mtoce <mtoce@villanova.edu>
2020-05-26 16:27:25 -0400

    web json updated

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, jsonify
 import json 
 from pdb import set_trace as st
+from os.path import join as join_path
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -11,12 +12,18 @@ home_routes = Blueprint("home_routes", __name__)
 def index():
     x = 2 + 2
     # return f"Hello World! {x}"
-    with open("web_app/db/strains.json") as f:
+    with open(join_path('web_app', 'db', 'strains.json')) as f:
         data = json.load(f)
     #breakpoint()
     # temp_var = json.loads("web_app/db/strains.json")
     return jsonify(data)
 
+@home_routes.route("/web_layout_strains")
+# creating new json object with 4 columns: name, desc, flavor, effect
+def four_col_format():
+    with open(join_path('web_app', 'db', 'for_brandon.json')) as f:
+        data = json.load(f)
+    return jsonify(data)
 
 @home_routes.route("/about")
 def about():
