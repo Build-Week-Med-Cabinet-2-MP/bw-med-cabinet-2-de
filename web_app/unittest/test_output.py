4165a4e1080f5292dbbf0eecead3b9aaeec8714d
mtoce <mtoce@villanova.edu>
2020-05-28 14:06:39 -0400

    Added 2 tests for testing input json object

--- web_app/unittest/test_output.py
+++ web_app/unittest/test_output.py
@@ -3,18 +3,30 @@ import requests
 import json
 from pdb import set_trace as st
 
-class TestData(unittest.TestCase):
+class TestDatatype(unittest.TestCase):
+    '''
+    Tests inputs and ouputs ensuring they are the correct type.
+    '''
 
-    def test_url(self):
+    def test_strains_type(self):
+        '''
+        Tests that the strains route url response is a json object
+        '''
         url = "http://127.0.0.1:5000/strains"
         response = requests.get(url)
         # st()
 
         self.assertIsInstance(response.json(), list)
 
-    def test_input(self):
-        #self.assertIsInstance(self.json_input, json)
-        pass
+    def test_strains_notnull(self):
+        '''
+        Tests that the strains route url response is not null
+        '''
+        url = "http://127.0.0.1:5000/strains"
+        response = requests.get(url)
+        # st()
+        self.assertTrue(response.json())
+        
     def test_output(self):
         pass
 
