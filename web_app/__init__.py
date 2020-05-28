340f2fadd64a9584cbd43c479335d3a9363eea19
mtoce <mtoce@villanova.edu>
2020-05-28 13:04:41 -0400

    cleaned up code to look nicer

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,19 +1,22 @@
 # web_app/__init__.py
 
 from flask import Flask
-
 from web_app.routes.home_routes import home_routes
-# from web_app.routes.book_routes import book_routes
-
 
 def create_app():
+    '''
+    Creates our flask app!
+    '''
     app = Flask(__name__)
+    # prevents jsonify from reordering our data in ways we don't want
     app.config['JSON_SORT_KEYS'] = False
+    # register the home_routes file 
     app.register_blueprint(home_routes)
-    # app.register_blueprint(book_routes)
+
     return app
 
 
 if __name__ == "__main__":
+    # instantiate the flask app
     my_app = create_app()
     my_app.run(debug=True)
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,10 +1,6 @@
 # web_app/routes/home_routes.py
 
 from flask import Blueprint, jsonify, request
-# from flask_migrate import Migrate
-# import psycopg2 as psy
-# from psycopg2.extras import execute_values
-# from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import json
 from pdb import set_trace as st
@@ -14,131 +10,21 @@ import pickle
 import numpy as np
 from web_app.ml_models.StrainRecommendation import StrainRecommendation as SR
 
+# Extra imports if neeeded
+# from flask_migrate import Migrate
+# import psycopg2 as psy
+# from psycopg2.extras import execute_values
+# from dotenv import load_dotenv
 # load_dotenv()
-home_routes = Blueprint("home_routes", __name__)
 # migrate = Migrate()
-
 # DATABASE_URL = os.getenv(DATABASE_URL, default="try_again")
 
-df_cols = ['Ammonia','Apple','Apricot','Berry','Blue Cheese','Blueberry','Cheese','Chemical','Chestnut','Citrus','Coffee','Diesel','Earthly','Flowery','Grape','Grapefruit','Honey','Lavender','Lemon','Lime','Mango','Mint','Nutty','Orange','Pepper','Pine','Pineapple','Plum','Pungent','Sage','Skunk','Spicy/Herbal','Strawberry','Sweet','Tar','Tea','Tobacco','Tree Fruit','Tropical','Vanilla','Woody','Aroused','Creative','Energetic','Euphoric','Focused','Giggly','Happy','Hungry','Relaxed','Sleepy','Talkative','Tingly','Uplifted','Hybrid','Indica', 'Sativa']
+# creates blueprint object from this file
+home_routes = Blueprint("home_routes", __name__)
 
-# get dict from ML guys
-# convert dict to .json object
-# backend can get .json from url/model
-@home_routes.route("/model", methods=["POST", "GET"])
-def final_output():
-    '''
-    Gets input data, feeds it to model, takes model output (df), converts to .json object and returns it
-    '''
-    input_json_obj = request.json
-    # input_json_obj = request.data TODO: this maybe it
-
-    # input_dict = receive_inputs()
-    encoded_input = input_encoder(input_json_obj, df_cols)
-
-    # ##### TODO: change this code to pickle and predict
-    # temp_file = open('examplePickle.pickl', 'rb')      
-    # model = pickle.load(temp_file) 
-    # temp_file.close()
-    # model_output_df = model.knn_predict(encoded_input)
-    # #####
-
-    model = SR()
-    prediction_df = model.knn_predict(encoded_input)
-    prediction_dict = prediction_df.to_dict(orient="records")
-    # st()
-
-    return jsonify(prediction_dict)
-
-    # json_output = dataframe_to_json(model_output_df)
-    # return json_output
-
-
-def receive_inputs():
-    '''
-    Takes form (.json object) from front-end and converts into dict for ML
-    '''
-    form_data = dict(request.form)
-    return form_data
-
-
-def input_encoder(input_dict, df_cols):
-    """
-    Takes in a dictionary of flavors and effects and a list of dataframe features.
-    Returns the input as a one-hot encoded list
-    """
-    # Convert dictionary values for both flavors and effects to a single list
-    input_list = [val for sublist in input_dict for val in input_dict[sublist]] 
-    # create a list of 0's with the same length as the dataframe features
-    one_hot_inputs = [0] * len(df_cols)
-    # Loop through list of features. If the feature is in our list of inputs, 
-    # assign the corresponding index in the one_hot_input list a value of 1.
-    for i in range(len(df_cols)):
-        if df_cols[i] in input_list:
-            one_hot_inputs[i] = 1
-    return np.array(one_hot_inputs)
-    # print(input_encoder(inputs, df_cols))
-
-
-def dataframe_to_json(df):
-    '''
-    converts a dataframe to a json object
-    '''    
-    # df = pd.DataFrame(dummy_decoded_output)
-    return jsonify(df.to_dict())
-
-
-@home_routes.route("/dummy_model", methods=["POST", "GET"])
-def dummy_final_output():
-    '''
-    returns a dummy output json
-    '''
-    # data = request.form["data_2"]
-    # st()
-    dummy_decoded_output = [
-        {
-            "Name": "Agent Tangie",
-            "Description": "Agent Tangie is a ...",
-            "Flavors": [
-                "Cirtus",
-                "Diesel",
-                "Earthy"
-            ],
-            "Effects": [
-                "Energetic",
-                "Focused",
-                "Giggly"
-            ]
-        }, {
-            "Name": "Name 2",
-            "Description": "Name 2 is a ...",
-            "Flavors": [
-                "flavor1",
-                "flavor2",
-                "flavor3"
-            ],
-            "Effects": [
-                "effects1",
-                "effects2",
-                "effects3"
-            ]
-        }, {
-            "Name": "Name 3",
-            "Description": "Name 3 is a ...",
-            "Flavors": [
-                "flavor1",
-                "flavor2",
-                "flavor3"
-            ],
-            "Effects": [
-                "effects1",
-                "effects2",
-                "effects3"
-            ]
-        }
-    ]
-    # st()
-    return jsonify(dummy_decoded_output)
+# columns from DF that will be used in the ML model
+df_cols = ['Ammonia', 'Apple', 'Apricot', 'Berry', 'Blue Cheese', 'Blueberry', 'Cheese', 'Chemical', 'Chestnut', 'Citrus', 'Coffee', 'Diesel', 'Earthly', 'Flowery', 'Grape', 'Grapefruit', 'Honey', 'Lavender', 'Lemon', 'Lime', 'Mango', 'Mint', 'Nutty', 'Orange', 'Pepper', 'Pine', 'Pineapple', 'Plum', 'Pungent',
+           'Sage', 'Skunk', 'Spicy/Herbal', 'Strawberry', 'Sweet', 'Tar', 'Tea', 'Tobacco', 'Tree Fruit', 'Tropical', 'Vanilla', 'Woody', 'Aroused', 'Creative', 'Energetic', 'Euphoric', 'Focused', 'Giggly', 'Happy', 'Hungry', 'Relaxed', 'Sleepy', 'Talkative', 'Tingly', 'Uplifted', 'Hybrid', 'Indica', 'Sativa']
 
 # route for strains (pre-data)
 @home_routes.route("/strains")
@@ -148,15 +34,13 @@ def index():
     '''
     with open(join_path('web_app', 'db', 'strains.json')) as f:
         data = json.load(f)
-    #breakpoint()
-    # temp_var = json.loads("web_app/db/strains.json")
     return jsonify(data)
 
 
 @home_routes.route("/web_layout_strains")
 def four_col_format():
     '''
-    Creatis new .json object with 4 columns: 
+    Creates new .json object with 4 columns:
     name, desc, flavor, effect
     '''
     with open(join_path('web_app', 'db', 'web_layout_strains.json')) as f:
@@ -164,13 +48,109 @@ def four_col_format():
     return jsonify(data)
 
 
+def input_encoder(input_dict, df_cols):
+    """
+    Takes in a dictionary of flavors and effects and a list of dataframe features.
+    Returns the input as a one-hot encoded list
+    """
+    # Convert dictionary values for both flavors and effects to a single list
+    input_list = [val for sublist in input_dict for val in input_dict[sublist]]
+    # create a list of 0's with the same length as the dataframe features
+    one_hot_inputs = [0] * len(df_cols)
+    # Loop through list of features. If the feature is in our list of inputs,
+    # assign the corresponding index in the one_hot_input list a value of 1.
+    for i in range(len(df_cols)):
+        if df_cols[i] in input_list:
+            one_hot_inputs[i] = 1
+    return np.array(one_hot_inputs)
+    # print(input_encoder(inputs, df_cols))
 
 
-######## TEST METHODS
-@home_routes.route("/dummy_model_TEST", methods=["GET"])
-def dummy_final_output_TEST(df):
+# POST: backend gives us the input .json object
+# GET: backend requests the .json output (recommendations) from ML model 
+@home_routes.route("/model", methods=["POST", "GET"])
+def final_output():
     '''
-    converts a dataframe to a json object
-    '''    
-    # df = pd.DataFrame(dummy_decoded_output)
-    return jsonify(df.to_dict())
+    Gets input data, feeds it to model, takes model output (df), converts to .json object and returns it
+    '''
+
+    # request .json from backend via their POST request
+    input_json_obj = request.json
+
+    # Run input encoder function on input .json object to get one-hot-encoded inputs for model
+    encoded_input = input_encoder(input_json_obj, df_cols)
+
+    # instantiates the Model Class
+    model = SR()
+    # Use method knn_predict from Model Class to predict one-hot-encoded input
+    prediction_df = model.knn_predict(encoded_input)
+    # transform df into dict, but make sure to orient=records so format is correct
+    prediction_dict = prediction_df.to_dict(orient="records")
+
+    # jsonify the dict so that backend/frontend can use it
+    return jsonify(prediction_dict)
+
+
+# BELOW IS CODE FOR DUMMY INPUTS & OUTPUTS TO MAKE SURE ROUTES FUNCTION PROPERLY
+#-------------------------------------------------------------------------------
+# @home_routes.route("/dummy_model", methods=["POST", "GET"])
+# def dummy_final_output():
+#     '''
+#     returns a dummy output json
+#     '''
+#     # data = request.form["data_2"]
+#     # st()
+#     dummy_decoded_output = [
+#         {
+#             "Name": "Agent Tangie",
+#             "Description": "Agent Tangie is a ...",
+#             "Flavors": [
+#                 "Cirtus",
+#                 "Diesel",
+#                 "Earthy"
+#             ],
+#             "Effects": [
+#                 "Energetic",
+#                 "Focused",
+#                 "Giggly"
+#             ]
+#         }, {
+#             "Name": "Name 2",
+#             "Description": "Name 2 is a ...",
+#             "Flavors": [
+#                 "flavor1",
+#                 "flavor2",
+#                 "flavor3"
+#             ],
+#             "Effects": [
+#                 "effects1",
+#                 "effects2",
+#                 "effects3"
+#             ]
+#         }, {
+#             "Name": "Name 3",
+#             "Description": "Name 3 is a ...",
+#             "Flavors": [
+#                 "flavor1",
+#                 "flavor2",
+#                 "flavor3"
+#             ],
+#             "Effects": [
+#                 "effects1",
+#                 "effects2",
+#                 "effects3"
+#             ]
+#         }
+#     ]
+#     # st()
+#     return jsonify(dummy_decoded_output)
+
+
+# # TEST METHODS
+# @home_routes.route("/dummy_model_TEST", methods=["GET"])
+# def dummy_final_output_TEST(df):
+#     '''
+#     converts a dataframe to a json object
+#     '''
+#     # df = pd.DataFrame(dummy_decoded_output)
+#     return jsonify(df.to_dict())
